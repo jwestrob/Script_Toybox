13dac8534bf4679bd05a24b36c475c8776df3712
Jacob West-Roberts <jwestrob@biotite.berkeley.edu>
2019-07-15 17:36:14 -0700

    parallelized remove_gaps_from_msa.py

--- remove_gaps_from_MSA.py
+++ remove_gaps_from_MSA.py
@@ -1,15 +1,29 @@
 from Bio import SeqIO
 import os, sys, pandas as pd
+from pathos.multiprocessing import ProcessingPool as Pool
 
 input_fasta = sys.argv[1]
 
-output_fasta = sys.argv[2]
+threads = sys.argv[2]
+
+#output_fasta = sys.argv[2]
 
 out_recs = []
 
+p = Pool(int(threads))
+
+
+def remove_gap(rec):
+	new_rec = rec
+	new_rec.seq = new_rec.seq.ungap('-')
+	return new_rec
+
+out_recs = list(p.map(remove_gap, SeqIO.parse(input_fasta, 'fasta')))
+
+"""
 for rec in SeqIO.parse(input_fasta, 'fasta'):
     new_rec = rec
     new_rec.seq = new_rec.seq.ungap('-')
     out_recs.append(new_rec)
-
-SeqIO.write(out_recs, output_fasta, 'fasta')
+"""
+SeqIO.write(out_recs, input_fasta, 'fasta')
