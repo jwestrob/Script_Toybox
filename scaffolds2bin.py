fa48015adb231483691723540cb0be74f5d479f6
jwestrob <cobe44@gmail.com>
2020-03-18 11:40:17 -0700

    generic nonsense

--- scaffolds2bin.py
+++ scaffolds2bin.py
@@ -7,6 +7,8 @@ from Bio.SeqRecord import SeqRecord
 import pandas as pd
 import argparse
 
+
+
 t1 = time.time()
 
 parser=argparse.ArgumentParser(description='Takes a series of bin files (fasta format) and contigs (fasta format), creates a tab-separated scaffolds2bin file for input to DAStool.')
@@ -18,6 +20,7 @@ parser.add_argument('-cd', metavar='contigs_dir', nargs='?', default=None, help=
 parser.add_argument('-r', action='store_true', default=False, help="Wanna turn a .scaffolds2bin file into a .fasta bin file?")
 parser.add_argument('-o', metavar='outfile', nargs='?', default=None, help="S2B ONLY: Name of outfile (.scaffolds2bin.tsv will be appended to whatever you put here)")
 parser.add_argument('-outdir', nargs='?', default='.', help="B2S only: Name of directory to throw your output FASTAs into.")
+parser.add_argument('-t', default=1, nargs='?', help='Number of threads to use.')
 
 args = parser.parse_args()
 
@@ -35,6 +38,10 @@ if args.o is not None:
 if args.outdir != '.':
     outdir = str(args.outdir).split('/')[0]
 reverse = args.r
+if args.t is not None:
+    threads = int(args.t)
+else:
+    threads = 1
 
 def main():
     if reverse == False:
@@ -78,33 +85,33 @@ def main():
             except:
                 print("Found some nonsense. Please evaluate: ", filename)
 
-        for binfile in binfile_list:
-            binfile_df = binfile[0]
-            binfile_name = binfile[1]
-
-            #Parse the right contigs file
-            contigs = list(SeqIO.parse(contig_file, 'fasta'))
-            print("Werkin on : ", binfile_name)
-            unique_bins = binfile_df.bin.unique()
-            for index, bin in enumerate(unique_bins):
-                #Get a reduced dataframe with only the rows corresponding to the bin in question
-                bin_red_df = binfile_df[binfile_df["bin"] == bin]
-                #Make an empty list to store SeqRecord objects to put in a FASTA
-                bin_records = []
-                for record in contigs:
-                    if record.id in list(bin_red_df["scaffold_name"]):
-                        bin_records.append(record)
-                if len(bin_records) == 0:
-                    print(binfile_name, bin)
-                    print(len(bin_red_df))
-                    print(bin_red_df.head())
-                    sys.exit()
-                SeqIO.write(bin_records, outdir + '/' + binfile_name + '-' + bin + '.fasta', 'fasta')
-                print('Wrote ' + binfile_name + ' bin ' + str(index) + ' to ' + binfile_name + '-' + bin + '.fasta')
+        p = Pool(threads)
+        dummy_list = p.map(fetch_recs_for_bin, binfile_list)
 
 
 
+def fetch_recs_for_bin(df_and_name):
+    binfile_df = df_and_name[0]
+    binfile_name = df_and_name[1]
 
+    #Parse the right contigs file
+
+    print("Werkin on : ", binfile_name)
+    unique_bins = binfile_df.bin.unique()
+    for index, bin in enumerate(unique_bins):
+        #Get a reduced dataframe with only the rows corresponding to the bin in question
+        bin_red_df = binfile_df[binfile_df["bin"] == bin]
+        bin_scaffolds = bin_red_df.scaffold_name.unique().tolist()
+        bin_records = list(filter(lamdba x: x.id in bin_scaffolds, SeqIO.parse(contig_file, 'fasta')))
+
+        if len(bin_records) == 0:
+            print(binfile_name, bin)
+            print(len(bin_red_df))
+            print(bin_red_df.head())
+            sys.exit()
+        SeqIO.write(bin_records, outdir + '/' + binfile_name + '-' + bin + '.fasta', 'fasta')
+        print('Wrote ' + binfile_name + ' bin ' + str(index) + ' to ' + binfile_name + '-' + bin + '.fasta')
+    return
 
 
 
